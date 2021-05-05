65d37dfe75909cd973843957aa320eb54e963ff7
Ravi9231 <83690162+Ravi9231@users.noreply.github.com>
2021-05-05 13:08:28 +0530

    Add files via upload
    latest one

--- Greet.java
+++ Greet.java
@@ -6,6 +6,7 @@ public class Greet {
 		System.out.println("Welcome to maven based core java project");
 		System.out.println("Welcome everyone in my code");
 		System.out.println("Now see the changes");
-		System.out.println("GIT USES");}
+		System.out.println("GIT USES");
+		System.out.println("NOW ITS WORKING");}
 
 }
