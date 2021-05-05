a9af98d5d51036ca43245336a514e6def95bf571
Ravi9231 <83690162+Ravi9231@users.noreply.github.com>
2021-05-05 13:04:14 +0530

    Add files via upload
    One more line added

--- Greet.java
+++ Greet.java
@@ -3,7 +3,7 @@ public class Greet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Wecome to maven based core java project");
-	}
+		System.out.println("Welcome to maven based core java project");
+		System.out.println("Wecome everyone in my code");	}
 
 }
